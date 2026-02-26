eaa414747ff96c48b7eed056339f3dd6008c264a
Aitor Salido Puga <aisapu@inspalamos.cat>
2026-02-26 17:55:15 +0100

    ok

--- projecte-Aitor-Final/src/components/Inici/Inici.jsx
+++ projecte-Aitor-Final/src/components/Inici/Inici.jsx
@@ -1,13 +1,52 @@
+import { Button } from "reactstrap";
 import styles from "./Inici.module.css";
 export default function Inici() {
   return (
     <>
-  <div>
-    <h1>Hola</h1>
-  </div>
-  <div>
-    <p>Adeu</p>
-  </div>
+    <section>
+      <div>
+        <img src="./img/miniTitolIcon.svg" alt="" />
+        <p>El teu garatge virtual</p>
+      </div>
+      <h1>Controla el teu vehicle.</h1>
+      <h1>Estalvia temps i diners.</h1>
+      <p>BoxSphere centralitza el manteniment, els gastos i el consum dels teus vehicles en una sola plataforma. Oblida't dels papers i les fulles de càlcul.</p>
+      <div>
+        <Button>Comença ara</Button>
+        <Button>Saber més</Button>
+      </div>
+      <img src="./img/dashboard.png" alt="" />
+      <div>
+        <p>Confien en nosaltres:</p>
+        <p>🚗 +500 vehicles</p>
+        <p>👤 +200 usuaris</p>
+        <p>⭐ 4.8/5 valoració</p>
+      </div>
+    </section>
+    
+    <section>
+      
+    </section>
+    
+    <section>
+      
+    </section>
+    
+    <section>
+      
+    </section>
+    
+    <section>
+      
+    </section>
+    
+    <section>
+      
+    </section>
+    
+    <section>
+      
+    </section>
   </>
   )
   
